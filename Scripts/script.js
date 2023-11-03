3cf3b63fed8d613c3724b1695e34365e5eadf2bd
AdrianNoces <142280131+AdrianNoces@users.noreply.github.com>
2023-11-03 06:28:01 +0000

    nice changes

---
+++ Scripts/script.js
@@ -0,0 +1,22 @@
+const exampleButton = document.getElementById("examplesButton");
+
+const contentDiv = document.getElementById("contentDiv");
+
+const homeButton = document.getElementById("homeButton");
+
+
+exampleButton.addEventListener("click", function () {
+  contentDiv.innerHTML = `
+  <li><a href="./Examples/Amazon/amazon.html">amazon</a></li>
+  <li><a href="">none</a></li>
+  <li><a href="">none</a></li>
+  <li><a href="">none</a></li
+
+  `
+})
+
+homeButton.addEventListener("click", function () {
+  contentDiv.innerHTML = `
+  <p>Underdevelopment</p>
+  `
+})
