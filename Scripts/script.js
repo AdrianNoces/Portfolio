4654fe0006a8eeb5a662f4bac8b6557913ae40ab
AdrianNoces <142280131+AdrianNoces@users.noreply.github.com>
2023-11-03 13:17:26 +0000

    k

--- Scripts/script.js
+++ Scripts/script.js
@@ -7,11 +7,10 @@ const homeButton = document.getElementById("homeButton");
 
 exampleButton.addEventListener("click", function () {
   contentDiv.innerHTML = `
-  <li><a href="./Examples/Amazon/index.html">amazon</a></li>
+  <li><a href="./Amazon/index.html">amazon</a></li>
   <li><a href="">none</a></li>
   <li><a href="">none</a></li>
   <li><a href="">none</a></li
-  li
 
   `
 })
