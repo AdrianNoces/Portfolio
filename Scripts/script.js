b777704fee39d2efbe598f2150eb6d5a11b9cd27
AdrianNoces <142280131+AdrianNoces@users.noreply.github.com>
2023-11-03 13:23:59 +0000

    little

--- Scripts/script.js
+++ Scripts/script.js
@@ -5,15 +5,9 @@ const contentDiv = document.getElementById("contentDiv");
 const homeButton = document.getElementById("homeButton");
 
 
-exampleButton.addEventListener("click", function () {
-  contentDiv.innerHTML = `
-  <p>Underdevelopment</p>
-
-  `
-})
 
 homeButton.addEventListener("click", function () {
   contentDiv.innerHTML = `
-  <p>Underdevelopment</p>
+  <p>Under Development</p>
   `
 })
